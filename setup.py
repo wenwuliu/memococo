5de5b4dcd9359f7b55b621c2348cd20cd83c5c2d
liuwenwu <liuawu625@163.com>
2025-03-11 11:44:50 +0800

    lost some file

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if current_os in OS_DEPENDENCIES:
 
 setup(
     name="MemoCoco",
-    version="2.1.30",
+    version="2.1.31",
     description="MemoCoco - 时间胶囊",
     long_description=long_description,
     long_description_content_type="text/markdown",
